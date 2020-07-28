b923ae5f20a8e03037020de4ee32f4a77d5e6b24
GonzalezL13 <luisgonzalez1331@icloud.com>
2020-07-28 16:09:58 -0700

    player constructers and prototypes added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -17,3 +17,25 @@ test("creates a player object", () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+//we're checking that player.getStats() returns an object with four specific properties
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+//On player creation, the inventory should already have something in it, 
+//so a call to player.getInventory() should return an array.
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+//if empty inventory needing to return false
+    expect(player.getInventory()).toEqual(false);
+});
+
